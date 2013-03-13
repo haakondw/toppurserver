f9dff615c6d4762416508a1893f9bf7d7f0ecbfd
Håkon <hdw@hdw-laptop.(none)>
2013-03-13 09:40:47 +0100

    attpåklatten

---
+++ src/dao/MedicineForm.java
@@ -0,0 +1,34 @@
+package dao;
+
+import java.io.Serializable;
+
+public class MedicineForm implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 7859719947157585234L;
+	private String form;
+
+	public MedicineForm(){}
+
+	public MedicineForm(String form){
+		this.form = form;
+	}
+
+	public String getForm(){
+		return form;
+	}
+	
+	public void setForm(String form){
+		this.form = form;
+	}
+	
+	@Override
+	public String toString() {
+		return "MedicineForm [form=" + form + "]";
+	}
+	
+	
+	
+}
