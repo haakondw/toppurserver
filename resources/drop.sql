e235cb321b4e50a422978a23850a98f21bb943db
Håkon <hdw@hdw-laptop.(none)>
2013-03-20 08:53:56 +0100

    sql scripts

---
+++ resources/drop.sql
@@ -0,0 +1,13 @@
+connect 'jdbc:derby://localhost:1527/toppurbase;user=user;password=pass';
+
+DROP TABLE department;
+DROP TABLE deviation;
+DROP TABLE emergency_contact;
+DROP TABLE medicine;
+DROP TABLE medicine_form;
+DROP TABLE pasient;
+DROP TABLE task;
+DROP TABLE user;
+DROP TABLE role;
+
+exit;
