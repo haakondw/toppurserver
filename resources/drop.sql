912b320e85b7e04453e9d7956f7bc2d2645a0e79
haakondw <haakon.dw@gmail.com>
2013-04-03 10:49:23 +0200

    sql scripts updated

--- resources/drop.sql
+++ resources/drop.sql
@@ -1,13 +1,13 @@
 connect 'jdbc:derby://localhost:1527/toppurbase;user=user;password=pass';
 
-DROP TABLE department;
 DROP TABLE deviation;
 DROP TABLE emergency_contact;
+DROP TABLE task;
 DROP TABLE medicine;
 DROP TABLE medicine_form;
 DROP TABLE pasient;
-DROP TABLE task;
-DROP TABLE user;
+DROP TABLE department;
+DROP TABLE app_user;
 DROP TABLE role;
 
 exit;
